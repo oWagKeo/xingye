4c1f8f628ac612257a52b74f528646550d2900cc
wangke <568768582@qq.com>
2018-01-24 17:28:24 +0800

    领取页面

--- www/application/index/controller/Index.php
+++ www/application/index/controller/Index.php
@@ -5,9 +5,17 @@ use think\Controller;
 
 class Index extends Controller
 {
-    public function index(){
+    /**
+     * QQ音乐礼包领取页面
+     */
+    public function qq_music(){
+        return $this->fetch();
+    }
 
-//        return $this->fetch();
-
+    /**
+     * 小红书礼包领取页面
+     */
+    public function red_book(){
+        return $this->fetch();
     }
 }
