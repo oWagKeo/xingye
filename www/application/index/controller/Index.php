1ed89bee5ae8b3bb24c2640b398e87452aa4baa5
wangke <568768582@qq.com>
2018-01-24 17:57:55 +0800

    修改图片

--- www/application/index/controller/Index.php
+++ www/application/index/controller/Index.php
@@ -18,4 +18,24 @@ class Index extends Controller
     public function red_book(){
         return $this->fetch();
     }
+
+    /**
+     * 接口
+     */
+    private function http_post($api,$p = ''){
+        $time = time();
+        $p = $p == '' ? '' : '&p='.$p;
+        $url = '';
+        $url .= '?time='.$time.'&sign='.md5('appkey='.$this->appkey.$p.'&time='.$time.'&key='.$this->md5key);
+        $url .= $p;
+        $ch = curl_init();
+        //设置选项，包括URL
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+
 }
